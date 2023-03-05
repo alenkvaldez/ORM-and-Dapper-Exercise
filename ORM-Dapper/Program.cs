a5e6ccdf0afa964e115a05e3debd6f47f11c1f05
Alen Valdez <116237490+alenkvaldez@users.noreply.github.com>
2023-03-05 13:53:53 -0800

    Update Program.cs

--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace ORM_Dapper
+using BestBuyPractices;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+string connString = config.GetConnectionString("DefaultConnection");
+
+IDbConnection conn = new MySqlConnection(connString);
+
+var departmentRepo = new DapperDepartmentRepository(conn);
+
+departmentRepo.InsertDepartment("alemans new department");
+
+var departments = departmentRepo.GetAllDepartments();
+
+foreach  (var department in departments)
 {
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+    Console.Write(department.Name);
+    Console.WriteLine(department.Name);
+    Console.WriteLine();
+    Console.WriteLine();
 }
